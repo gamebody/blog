b074444839c9daf9b6d0c95a3a2b56e47e6ba4ae
gamebody <atme_talk@163.com>
2017-04-20 09:17:15 +0800

    curry

--- javascript函数式编程/src/add.js
+++ javascript函数式编程/src/add.js
@@ -1,17 +1,39 @@
-function add(a, b) {
-  switch (arguments.length) {
-    case 0:
-      return add
-    case 1:
-      return function (b) {
-        return a + b
-      }
-    case 2:
-      return a + b
+// 最简版本
+// function add(a, b) {
+//   switch (arguments.length) {
+//     case 0:
+//       return add
+//     case 1:
+//       return function (b) {
+//         return a + b
+//       }
+//     case 2:
+//       return a + b
+//   }
+// }
+
+// const num1 = add(2, 3)
+// const num2 = add(2)(3)
+
+// console.log(num1, num2)
+function _curry2 (fn) {
+  return function f (a, b) {
+    switch (arguments.length) {
+      case 0:
+        return f
+      case 1:
+        return function(_b) { return fn(a, _b) }
+      case 2:
+        return fn(a, b)
+    }
   }
 }
 
-const num1 = add(2, 3)
-const num2 = add(2)(3)
+const add = _curry2(function add (a, b) {
+  return a + b
+})
 
-console.log(num1, num2)
+console.log(add(2, 3))
+console.log(add(2)(3))
+
+
